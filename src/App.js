3166d88ac40b50d3d2f98e965b78908ff8e6a99a
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-08-05 02:02:20 +0200

    Layout structure completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import UserIP from './components/UserIP';
+import { Container } from '@mui/system';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container sx={{
+        // marginY: 5,
+        textAlign: 'center',
+        background: "#fff",
+        height: "100vh"
+        }}>
+        <h1 className='hello'>Hello, and welcome to the IP Finder</h1>
+        <div className='ip'>
+          <h3>Want to know your IP?</h3>
+          <p>Kindly click on the button below...👇🏾</p>
+        </div>
+        <Link to="user_ip"><button className='getIpBtn'>Get Your IP</button></Link>
+      </Container>
   );
 }
 
---
+++ src/components/UserIP.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import './UserIP.css'
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import {AccessTime, CalendarMonth} from "@mui/icons-material";
+import {createTheme, ThemeProvider} from "@mui/material";
+import { Link } from 'react-router-dom';
+
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: "body1"
+                    },
+                    style: {
+                        fontSize: 12,
+                    }
+                },
+                {
+                    props: {
+                        variant: "body2"
+                    },
+                    style: {
+                        fontSize: 9,
+                    }
+                },
+                {
+                    props: {
+                        variant: "body3"
+                    },
+                    style: {
+                        fontSize: 7,
+                    }
+                }
+            ]
+        }
+    }
+})
+
+export default function UserIP() {
+  return (
+        <Paper elevation={3} className="paper" sx={{marginY: 5}}>
+            <Link to="/"><button className='goBack'>Go back</button></Link>
+            <h1>What's My IP?</h1>
+            <img src='./long mic.png'
+            className='bg-image'/>
+            <Paper elevation={6} className='floatPaper'>
+                <img src='./radio.jpg'
+                className='float-image'/>
+                <Box className='myBox'>
+                    <Box>
+                        <Typography variant='body1' className='boldFont'>
+                            <p>Your IP adress is 78.337.6.1</p>
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant='body3'>
+                            <p className='pBold'>You are currently located in Siegen, Germany</p>
+                        </Typography>
+                    </Box>
+                    <Box className='boxLine'>
+                        <CalendarMonth  sx={{width: 17}}/>
+                        <Typography variant='body2' marginLeft={0.5}>
+                            Dated on the 4.08.2022
+                        </Typography>
+                    </Box>
+                    <Box className='flexBox'>
+                        <AccessTime  sx={{width: 17}}/>
+                        <Typography marginLeft={0.5} variant='body2'>
+                            Your local time is 15:12
+                        </Typography>
+                    </Box>
+                </Box>
+            </Paper>
+        </Paper>
+  )
+}
